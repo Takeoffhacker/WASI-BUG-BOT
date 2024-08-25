7620d47e18b8bc7683fce97636ad154175706691
Takeoffhacker <166225930+Takeoffhacker@users.noreply.github.com>
2024-08-25 00:56:19 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "23288429245"
+global.ownername = "Takeoff "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '23288429245'  //creator number
+global.ownername = ' Mohamed' //owner name
+global.botname = 'takeoff_bot ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
